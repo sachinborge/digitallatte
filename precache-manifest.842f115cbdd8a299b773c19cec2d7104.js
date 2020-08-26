7e945814ff17a67bddc70f734289cba94b52c678
Sachin <borgedadaso50@gmail.com>
2020-08-26 15:44:20 +0530

    Updates

---
+++ precache-manifest.842f115cbdd8a299b773c19cec2d7104.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f923f1fd9263cdfb2019d18372d49f6",
+    "url": "/digitallatte/index.html"
+  },
+  {
+    "revision": "718e781ff66d9fcdece6",
+    "url": "/digitallatte/static/css/2.f9e7dec3.chunk.css"
+  },
+  {
+    "revision": "dbdc0138129cc109eb8e",
+    "url": "/digitallatte/static/css/main.75e05486.chunk.css"
+  },
+  {
+    "revision": "718e781ff66d9fcdece6",
+    "url": "/digitallatte/static/js/2.c526789c.chunk.js"
+  },
+  {
+    "revision": "d0d6b5033fd77eb6038046252d67d3b7",
+    "url": "/digitallatte/static/js/2.c526789c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbdc0138129cc109eb8e",
+    "url": "/digitallatte/static/js/main.6f59c110.chunk.js"
+  },
+  {
+    "revision": "c20bcf11fd631b3d2042",
+    "url": "/digitallatte/static/js/runtime-main.9b8741ff.js"
+  },
+  {
+    "revision": "62b11b0a31ad38d13aee58acf2515b23",
+    "url": "/digitallatte/static/media/case_study1.62b11b0a.jpg"
+  },
+  {
+    "revision": "2826876057f79fc6f1e48484eecb4405",
+    "url": "/digitallatte/static/media/case_study2.28268760.jpg"
+  },
+  {
+    "revision": "cb53bbaa04d7a6532557a544e5fd7bad",
+    "url": "/digitallatte/static/media/coffee-cup.cb53bbaa.gif"
+  },
+  {
+    "revision": "7e729274a43c94985fe4f497133420ca",
+    "url": "/digitallatte/static/media/counter_bg.7e729274.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/digitallatte/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/digitallatte/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/digitallatte/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/digitallatte/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/digitallatte/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f13d5381138597093b179cf55c890207",
+    "url": "/digitallatte/static/media/footer_logo.f13d5381.png"
+  },
+  {
+    "revision": "bfc7e99772b465ce1fc9ee91517b5249",
+    "url": "/digitallatte/static/media/text-gif.bfc7e997.gif"
+  },
+  {
+    "revision": "45806c9556333d1de883ae453ebc9b3b",
+    "url": "/digitallatte/static/media/who-we-are-1.45806c95.png"
+  },
+  {
+    "revision": "62e6288ffd8b77166705010259da4294",
+    "url": "/digitallatte/static/media/who-we-are-2.62e6288f.png"
+  }
+]);
